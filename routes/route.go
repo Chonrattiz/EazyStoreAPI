a596a66b08f7758ffb3efcaf9c7237d950e30c29
Poder2004 <66011212129@msu.ac.th>
2026-07-12 17:29:24 +0700

    fix: update dependencies and improve CORS configuration in router setup

--- routes/route.go
+++ routes/route.go
@@ -2,12 +2,23 @@ package routes
 
 import (
 	"EazyStoreAPI/middleware"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	//  Public (ไม่ต้องใช้ Token)
 	authGroup := r.Group("/api/auth")
